5f236293bf00f154b169424f60c96a88aad9bfb1
Priyansh Jain <48293372+ErR0rpj@users.noreply.github.com>
2020-08-10 13:28:19 +0530

    Authentication for Email, Google, Facebook created. Phone number authentication left.

--- app/src/main/java/com/example/stockmoney/MainActivity.java
+++ app/src/main/java/com/example/stockmoney/MainActivity.java
@@ -1,30 +1,145 @@
 package com.example.stockmoney;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG =MainActivity.class.getSimpleName();
+
+    //Variables for firebase authentication
+    public static final int RC_SIGN_IN = 1;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    //Information of users which are used everywhere in the app
+    public static String Username = "";
+    public static int funds = 0;
+    public static String email = "";
+    public static String uid = "";
+
+    //variablesof firebase database
+    public static FirebaseDatabase mFirebaseDatabase;
+    public static DatabaseReference mDatabaseReference;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Checking internet connection, permission added in manifest-file...code starting.
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected =activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        Log.e(LOG_TAG, "Internet Connection = " + isConnected);
+
+        if(!isConnected) {
+            Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_SHORT).show();
+        }
+        //Checking internet connection....code finished.
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase =FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("users");
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // menu should be considered as top level destinations. ye maine nhi likha h @ojha
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_watchlist, R.id.navigation_portfolio, R.id.navigation_search, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        //FirebaseUi and authentication...code starting
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+
+                if(user != null){
+                    email = user.getEmail();
+                    uid = user.getUid();
+                    onSignedInInitialize(user.getDisplayName());
+                }
+                else {
+                    onSignedOutCleanup();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                                    new AuthUI.IdpConfig.FacebookBuilder().build()))
+                            .build(),
+                    RC_SIGN_IN);
+                }
+            }
+        };
+        //FirebaseUI and authentication...code end
+
     }
 
+    private void onSignedInInitialize(String username){
+        Username = username;
+    }
+
+    //After logout cleaning up the variables
+    private void onSignedOutCleanup(){
+        Username = "";
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    //Initializing all the variables of the user and storing it in firebase
+    //This is not yet implemented.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(this, "Signed In Successfully", Toast.LENGTH_SHORT).show();
+            }
+            else if(resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "Sign In Cancelled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
 }
