1fcf3421fe5070b075ae57bd5cbed4cb45d32e62
Priyansh Jain <48293372+ErR0rpj@users.noreply.github.com>
2020-10-18 11:34:59 +0530

    Error resolved in profileUI

--- app/src/main/java/com/example/stockmoney/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/stockmoney/ui/profile/ProfileFragment.java
@@ -4,33 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
-import androidx.lifecycle.ViewModelProviders;
-
 import com.example.stockmoney.R;
 
 public class ProfileFragment extends Fragment {
 
-    private ProfileViewModel profileViewModel;
+    private final String LOG_TAG = ProfileFragment.class.getSimpleName();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_profile);
-        profileViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return view;
     }
 }
