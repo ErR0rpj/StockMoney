92c3e91737fcca43acf3c419675c289650df3b03
Priyansh Jain <48293372+ErR0rpj@users.noreply.github.com>
2020-10-22 22:39:01 +0530

    sell function created but not tested

--- app/src/main/java/com/example/stockmoney/services/Transactions.java
+++ app/src/main/java/com/example/stockmoney/services/Transactions.java
@@ -59,4 +59,46 @@ public class Transactions {
 
         Toast.makeText(activity, "Bought Successfully", Toast.LENGTH_SHORT).show();
     }
+
+
+    //TODO: Fetch latest data from firebase for the current stock and pass here.
+    public void sell(double currentPrice, String symbol, int quantity, Activity activity, Context context){
+        if(quantity < 1) {
+            Toast.makeText(activity, "Quantity cannot be 0", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //TODO: Take this code separate in a new class.
+        //TODO: Call the below code after the button is pressed and before coming in this function to reduce number of arguments.
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        Log.e("Transactions: ", "Internet Connection = " + isConnected);
+
+        if(!isConnected) {
+            Toast.makeText(activity, "No Internet Connection! Unable to Sell.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //TODO: Fetch /stocksOwn/symbol from firebase and then change that class accordingly.
+        //TODO: check if the user have enough quantity of current stock.
+
+        double funds = MainActivity.currentUser.getFunds();
+        funds = funds + (currentPrice*quantity);
+
+        //Change below lines to fetch data from firebase.
+        StocksOwn currentOwned = new StocksOwn(currentPrice, "edit", quantity, symbol);
+
+        //TODO: Take this code out in a new class.
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("users").child(MainActivity.currentUser.getUid()).child("stocksOwn").child(symbol);
+        mDatabaseReference.setValue(currentOwned);
+
+        MainActivity.currentUser.setFunds(funds);
+        mDatabaseReference = mFirebaseDatabase.getReference().child("users").child(MainActivity.currentUser.getUid());
+        mDatabaseReference.child("funds").setValue(funds);
+
+        Toast.makeText(activity, "Bought Successfully", Toast.LENGTH_SHORT).show();
+    }
 }
